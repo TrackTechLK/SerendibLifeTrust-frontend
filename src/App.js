fadc4012110c43612eac56f87d2964d96d0a3561
eshans <eshans2000@gmail.com>
2023-02-09 14:39:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Admin, Delete, Resource, ListGuesser, EditGuesser } from "react-admin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import drfProvider from "ra-data-django-rest-framework";
+
+const dataProvider = drfProvider("http://localhost:8000");
+
+// import { PostList } from './posts'
+
+class App extends Component {
+  render() {
+    return (
+      <Admin dataProvider={dataProvider}>
+        {/* <Resource
+          name="posts"
+          list={PostList}
+        /> */}
+        <Resource name="users" list={<ListGuesser />} edit={EditGuesser} />
+      </Admin>
+    );
+  }
 }
 
 export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
